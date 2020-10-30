bd385b02dbf09b62e8f88281d0e90dbbef778ad8
shivaniparnerkar <shivaniparnerkar15@gmail.com>
2020-10-30 17:27:20 -0400

    Data Model Commit

---
+++ src/main/java/com/example/msscliquorservice/domain/Beer.java
@@ -0,0 +1,46 @@
+package com.example.msscliquorservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GeneratorType;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name="UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36,columnDefinition = "varchar", nullable = false, updatable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+    private BigDecimal price;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
